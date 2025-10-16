b7d8c6bc3fbc05ac6b6fd2cd939c7375a86dda96
emid1-o <emidiolop5@gmail.com>
2025-10-16 11:18:59 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import layoutDefault from "./layouts/layoutDefault.vue";
+import cardTeste from "./components/cardTeste.vue";
 </script>
 
 <template>
   <layout-default />
+  <cardTeste/>
 </template>
 
 <style scoped></style>
